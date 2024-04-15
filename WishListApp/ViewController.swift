1dc79e90c44c186afdc6117b48fe625d1eb18597
신승현 <sinseunghyeon313@gmail.com>
2024-04-15 19:46:17 +0900

    Initial Commit

---
+++ WishListApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WishListApp
+//
+//  Created by t2023-m0056 on 4/15/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
